aa8f64dc2cf2b28f78917e0ef243d71118884f10
wuwenbin <zhangjianfei.com>
2019-05-17 17:53:50 +0800

    静态资源打包

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 var n=document.getElementById("root")
 var r=document.createElement("div")
 import c from '../img/webLogo.png'
+import d from '../img/webpackLogo.png'
+import e from '../img/12.jpg'
+import f from '../img/gif.gif'
+import './index.css'
 r.innerText="webpack"
 n.append(r)
 var img = document.createElement("img")
+var img2 = document.createElement("img")
+var img3 = document.createElement("img")
 img.src = '../img/webLogo.png';
+img2.src = e;
+img3.src = f;
+//样式  h5新特性  给节点添加class名  对应的还有clssList.add 添加  .remove 删除  .toggle 替换
+img.classList.add('imgOne');
+img2.classList.add('imgTwo');
+img3.classList.add('imgThree');
+
+
 n.append(img);
+n.append(img2);
+n.append(img3);
--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,22 @@ module.exports = {
 	},
 	module: {
 		rules: [{
-			test: /\.png$/,
+			test: /\.(png|jpg|gif)$/,
 			use:{
-				loader: 'file-loader'
+				loader: 'url-loader',
+				options:{
+					//placeholder 占位符
+					//name:'[name].[ext]'	//打包后保留原始图片的名称
+					// name: '[name]-[hash].[ext]' // 加上hsash值
+					//name: '[path][name].[ext]',//创建专门的文件存储
+					name: '[name].[ext]',//再或者你可以指定文件夹名称去打包
+					outputPath:'imges/',
+					limit: 204800 // 20KB
+				}
 			}
+		},{
+			test: /\.css$/,
+			use:['style-loader', 'css-loader']
 		}]
 	}, 
 	output: {//出口 打包后的index.js文件名字和所在的文件夹
